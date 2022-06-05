7bf7057e372f9a7b7fc0d7cc75569f6f1cf94eee
Arthur Hülle Leite <arthurleitee@gmail.com>
2022-06-05 18:55:47 -0300

    fix: login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,36 +4,38 @@ import api from '../../services/axios';
 import React, { useState } from 'react';
 import { Footer } from '../../components/Footer';
 const Login = () => {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-   
-    const handleSubmit = async (event:React.FormEvent) => {
-        event.preventDefault();  
-    }   
-    
-    return (
-        <div className="container">   
-            <Header/>
-            <div className="content">
-                <form onSubmit={handleSubmit} className="form">
-                    <div className="form__group">
-                        <label>Email</label>
-                        <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} required/>
-                    </div>
-                    <div className="form__group">
-                        <label>Senha</label>
-                        <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} required/>
-                    </div>
-                    <div className="form__group">
-                        <button type='submit' className="form__btn">Login</button>
-                    </div>
-                </form>
-                <a className="signup"href="/">Não possui conta? Cadastre-se aqui</a>
-            </div>
-            <Footer/>
-        </div>
-    )
+
+	const handleSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+	}
+
+	return (
+		<div className="container">
+			<Header />
+			<div className="content">
+				<form onSubmit={handleSubmit} className="form">
+					<label>Email</label>
+					<div className="form__group">
+
+						<input type="text" onChange={(e) => setEmail(e.target.value)} value={email} required />
+					</div>
+					<label>Senha</label>
+					<div className="form__group">
+
+						<input type="password" onChange={(e) => setPassword(e.target.value)} value={password} required />
+					</div>
+					<div className="form__group">
+						<button type='submit' className="form__btn">Login</button>
+					</div>
+				</form>
+				<a className="signup" href="/">Não possui conta? Cadastre-se aqui</a>
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
 export default Login;
