3bfa75aa9efae0957a4546b8782f2ab53530fac0
kaiquen <kaiquenascente@gmail.com>
2022-06-05 18:15:31 -0300

    refactor: change login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,24 +1,40 @@
 import { Header } from '../../components/Header';
 import './styles.scss';
-
+import api from '../../services/axios';
+import React, { useEffect, useState } from 'react';
 const Login = () => {
-    return (
-        <>
-            <Header />
-            <div className="container">
-                <div className="container__login">
-                    <p>Email</p>
-                    <input type="text" />
-                    <p>Senha</p>
-                    <input type="text" />
-                    <button className="container__login__btn">
-                        Login
-                    </button>
-                    <a href="/">Não possui conta? Cadastre-se aqui</a>
-                </div>
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
 
+   
+    const handleSubmit = async (e:any) => {
+        e.preventDefault();
+        console.log(e)
+        
+    }   
+    
+    return (
+        <div className="container">
+            <Header />   
+            <div className="content">
+                <div className="formBox">
+                    <form onSubmit={handleSubmit} className="form">
+                        <div className="form__group">
+                            <label>Email</label>
+                            <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} required/>
+                        </div>
+                        <div className="form__group">
+                            <label>Senha</label>
+                            <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} required/>
+                        </div>
+                        <div className="form__group">
+                            <button type='submit' className="form__btn">Login</button>
+                        </div>
+                    </form>
+                    <a className="signup"href="/">Não possui conta? Cadastre-se aqui</a>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
